8d3109c49a9e3b75427da1cd30d3347f9d4c7ecb
chaker ramdani <limio.viviok@gmail.com>
2023-11-23 12:43:46 +0100

    folders added

---
+++ src/App.js
@@ -0,0 +1,178 @@
+import "./index.css";
+import logo from "./images/logo.svg";
+import { useState } from "react";
+
+export default function App() {
+  const [bill, setBill] = useState(0);
+  const [percentage, setPercentage] = useState("");
+  const [numPeople, setNumPeople] = useState(0);
+
+  let tipAmount = percentage / numPeople;
+  let totalAmount = (bill + percentage) / numPeople;
+
+  function handleClick(e) {
+    setPercentage(Number(e.target.value.slice(1)));
+  }
+
+  function handleReset() {
+    setBill(0);
+    setPercentage("");
+    setNumPeople(0);
+  }
+
+  return (
+    <div className="container">
+      <div className="logo">
+        <img src={logo} alt="logo" />
+      </div>
+      <div className="card">
+        <Bill
+          bill={bill}
+          setBill={setBill}
+          numPeople={numPeople}
+          setNumPeople={setNumPeople}
+          percentage={percentage}
+          setPercentage={setPercentage}
+          onSelectPercentage={handleClick}
+          onReset={handleReset}
+        />
+        <Result
+          bill={bill}
+          tipAmount={tipAmount}
+          totalAmount={totalAmount}
+          numPeople={numPeople}
+          onReset={handleReset}
+        />
+      </div>
+    </div>
+  );
+}
+
+function Bill({
+  bill,
+  setBill,
+  numPeople,
+  setNumPeople,
+  onSelectPercentage,
+  percentage,
+  setPercentage,
+}) {
+  return (
+    <div className="bill">
+      <div className="bill-info separator">
+        <label>Bill</label>
+        <input
+          type="text"
+          value={bill}
+          onChange={(e) => setBill(Number(e.target.value))}
+        />
+      </div>
+      <div className="tip-info separator">
+        <label>Select Tip %</label>
+        <div className="inputs" value={percentage}>
+          <input type="text" value="$5" readOnly onClick={onSelectPercentage} />
+          <input
+            type="text"
+            value="$10"
+            readOnly
+            onClick={onSelectPercentage}
+          />
+          <input
+            type="text"
+            value="$15"
+            readOnly
+            onClick={onSelectPercentage}
+          />
+          <input
+            type="text"
+            value="$25"
+            readOnly
+            onClick={onSelectPercentage}
+          />
+          <input
+            type="text"
+            value="$50"
+            readOnly
+            onClick={onSelectPercentage}
+          />
+          <input
+            className="special"
+            type="text"
+            placeholder="Custom"
+            onChange={(e) => {
+              setPercentage(Number(e.target.value));
+            }}
+          />
+        </div>
+      </div>
+      <div className="people-info separator">
+        <span className="number">
+          <label>Number of Peaple</label>
+          <span
+            className="message"
+            style={numPeople === 0 ? { display: "block" } : { display: "none" }}
+          >
+            Can't be zero
+          </span>
+        </span>
+        <input
+          style={numPeople === 0 ? { outline: "2px solid #ff0000d9" } : {}}
+          type="text"
+          value={numPeople}
+          onChange={(e) => {
+            if (!bill || !percentage) return;
+            setNumPeople(Number(e.target.value));
+          }}
+        />
+      </div>
+    </div>
+  );
+}
+
+function Result({ tipAmount, totalAmount, numPeople, onReset }) {
+  return (
+    <div className="result">
+      <div className="separator tip-result">
+        <div className="parent">
+          <label>Tip Amount</label>
+          <span>/ person</span>
+        </div>
+        <input
+          type="text"
+          value={
+            numPeople > 0
+              ? `${
+                  Number.isInteger(tipAmount)
+                    ? `$${tipAmount}.00`
+                    : `$${tipAmount}`
+                }`
+              : "$0.00"
+          }
+          readOnly
+        />
+      </div>
+
+      <div className="total-result">
+        <div className="parent">
+          <label> Total</label>
+          <span>/ person</span>
+        </div>
+        <input
+          type="text"
+          value={
+            numPeople > 0
+              ? `${
+                  Number.isInteger(totalAmount)
+                    ? `$${totalAmount}.00`
+                    : `$${totalAmount}`
+                }`
+              : "$0.00"
+          }
+          readOnly
+        />
+      </div>
+
+      <button onClick={onReset}>Reset</button>
+    </div>
+  );
+}
